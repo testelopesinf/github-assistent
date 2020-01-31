0f6e8344ee8fe2e182bf339b5d37df1d52a3df58
Leonardo Lopes <leonardolopesinf@gmail.com>
2020-01-31 03:03:21 -0300

    ci(mobile): starting application
    starting the react native application using expo
    
    close #1

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Github Assitent</Text>
+    </View>
+  );
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
